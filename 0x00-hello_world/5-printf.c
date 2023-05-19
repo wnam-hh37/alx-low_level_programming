f9e16793ae56611bbd0f86507f54ad704b8cd18d
wnam-hh37 <iidujl55@gmail.com>
2023-05-19 08:08:26 -0700

    6-size.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
 /*
- * main - A program that prints a line with printf command
- * Return: 0 (Success)
+ * main - A program that prints a line using the  printf function
  */
-int main(void)
+void main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
