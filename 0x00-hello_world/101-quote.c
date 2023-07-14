ca0070c859bbf8952cdcf3dd505bb8ebb1087418
wnam-hh37 <102317612+wnam-hh37@users.noreply.github.com>
2023-07-14 21:55:27 +0000

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
-#include <unixstd.h>
+#include <unistd.h>
 /**
- * main - A  program that prints a line to the standard error
- * Return: 1 (success)
+ * main - Entry point
+ * Return:1
  */
 int main(void)
 {
-fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
 return (1);
 }
